15902a0436ef247ab535ac9e46159f4d5084cc3b
Arpit Gupta <arpitrgupta19@gmail.com>
2024-07-26 22:24:28 +0530

    Add functionality to add two numbers

---
+++ add.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Sum is: " << (a + b) << "\n";
+}
